39ef307e202b71ff8dc14203a81f2a82943f15df
Atif Pervaiz <mohammadatif50@gmail.com>
2020-12-04 11:50:13 +0500

    migrate to androidX
    migrate to androidX, update libraries

--- circularimageviewlib/src/main/java/com/blogspot/atifsoftwares/circularimageview/CircularImageView.java
+++ circularimageviewlib/src/main/java/com/blogspot/atifsoftwares/circularimageview/CircularImageView.java
@@ -12,7 +12,7 @@ import android.graphics.Paint;
 import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.v7.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import static android.widget.ImageView.ScaleType.CENTER_CROP;
 import static android.widget.ImageView.ScaleType.CENTER_INSIDE;
